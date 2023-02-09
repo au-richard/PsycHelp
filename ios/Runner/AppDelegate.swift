d83a96e0a24603221251120ab88367da499de83c
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2023-02-08 18:10:54 -0800

    update file paths with API KEY placeholder

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDNf3GMs-4P_pbSflPxoBFaToeVqwA87KA")
+    GMSServices.provideAPIKey("API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
